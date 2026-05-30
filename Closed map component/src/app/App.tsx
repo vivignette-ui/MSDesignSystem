899f5618226338661a1121ff029ccbdfd4bc3e73
vivignette-ui <vi.vignette@gmail.com>
2026-05-29 23:58:28 -0400

    Add files via upload

---
+++ Closed map component/src/app/App.tsx
@@ -0,0 +1,11 @@
+import ClosedMap from "../imports/ClosedMap/ClosedMap";
+
+export default function App() {
+  return (
+    <div className="size-full flex items-center justify-center bg-[#0a0e1a]">
+      <div className="w-[379px] h-[153px]">
+        <ClosedMap />
+      </div>
+    </div>
+  );
+}
---
+++ open map/src/app/App.tsx
@@ -0,0 +1,11 @@
+import ClosedCard from "./components/ClosedCard";
+
+export default function App() {
+  return (
+    <div className="size-full flex items-center justify-center bg-[#0a0e1a]">
+      <div className="w-[379px] h-[190px]">
+        <ClosedCard />
+      </div>
+    </div>
+  );
+}
